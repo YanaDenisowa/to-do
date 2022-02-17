933fb52a10a39b63df2eecd52d1770a7de75277a
yana <yanags1990@gmail.com>
2022-02-17 16:49:41 +0200

    Change structure

--- scripts/ClassLocalStorageHelper.js
+++ scripts/ClassLocalStorageHelper.js
@@ -1,4 +1,33 @@
 class LocalStorageHelper {
+    static getUsers() {
+        return (localStorage.length) ? JSON.parse(localStorage.getItem("users")) : [];
+    }
+
+    static  addNewUser(user) {
+        let usersFromStorage = this.getUsers();
+        usersFromStorage.push(user);
+        localStorage.setItem("users", JSON.stringify(usersFromStorage));
+
+
+    }
+
+    static checkUserExist (user) {
+        // let usersFromStorage = this.getUsers();
+        // if (localStorage.getItem('email') !== null &&
+        //     localStorage.getItem(user['password']) !== null) {
+        //
+        //     console.log(`Email address and password exists`);
+        // } else {
+        //     console.log(`Email address not found --- This is a new user`);
+        // }
+
+        // if (localStorage.getItem("userEmail") === null) {
+        //     console.log("No such email");
+        // }
+
+    }
 
 
 }
+
+
--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,66 @@
 // take data from inputs
+function checkUser() {
+    event.preventDefault();
+    let email = document.getElementById('email');
+    let password = document.getElementById('password');
+
+    let userEmail = email.value;
+    let userPassword = CryptoJS.MD5(password.value).toString();
+// console.log(userEmail);
+
+
+   LocalStorageHelper.getUsers().forEach((element) => {
+        console.log(element);
+        if (userEmail === element.email) {
+            if(userPassword === element.password){
+                let loginFrom = document.querySelector('.login__block');
+                loginFrom.style.display = "none";
+                let todoBlock = document.querySelector('.todo__block');
+                todoBlock.style.display = "block";
+
+
+            }
+        }
+
+    })
+
+
+
+}
+
 
 
 function registerUser(){
     event.preventDefault();
 
-let password = document.getElementById('password');
-let email = document.getElementById('email');
+    let password = document.getElementById('password');
+    let email = document.getElementById('email');
 
-let userEmail = email.value;
-let userPassword = CryptoJS.MD5(password.value).toString();
-
-//Add newUser-example Class of user
-let newUser = new User(userEmail, userPassword);
+    let userEmail = email.value;
+    let userPassword = CryptoJS.MD5(password.value).toString();
 
 
-console.log(userEmail, userPassword);
-console.log(password, email);
-console.log(newUser);
+    //Add newUser-example Class of user
+    let newUser = new User(userEmail, userPassword);
+
+    let newLocalStorageHelper = new LocalStorageHelper();
+
+    //add new user to local storage
+    LocalStorageHelper.addNewUser(newUser);
+
+    console.log(LocalStorageHelper.getUsers());
+
+    LocalStorageHelper.checkUserExist(newUser);
+
 
 }
+
+// function clearInputs(){
+//     let password = document.getElementById('password');
+//     let email = document.getElementById('email');
+//     let userEmail = email.value;
+//     let userPassword = CryptoJS.MD5(password.value).toString();
+//     userEmail = "";
+//     userPassword= "";
+//     console.log('text clear');
+// }
